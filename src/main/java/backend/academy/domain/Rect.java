9da6f9b37ca98d027c90a61501981da8fa1a223d
shcherbanev <shcherbanev1@yandex.ru>
2024-11-25 15:11:47 +0300

    fix: fix bug with creating new Pixel instead of getting from matrix

--- src/main/java/backend/academy/domain/Rect.java
+++ src/main/java/backend/academy/domain/Rect.java
@@ -6,9 +6,9 @@ public record Rect(double xMin, double yMin, double xMax, double yMax) {
         return (point.x() >= xMin && point.x() <= xMax) && (point.y() >= yMin && point.y() <= yMax);
     }
 
-    public Pixel mapPointToPixel(Point point , int imageWidth, int imageHeight) {
-        double mappedX = (xMax - point.x()) / (xMax - xMin) * imageWidth;
-        double mappedY = (yMax - point.y()) / (yMax - yMin) * imageHeight;
-        return new Pixel((int) mappedX, (int) mappedY);
+    public Pixel mapPointToPixel(FractalImage image, Point point , int imageWidth, int imageHeight) {
+        int mappedX = (int) ((xMax - point.x()) / (xMax - xMin) * imageWidth);
+        int mappedY = (int) ((yMax - point.y()) / (yMax - yMin) * imageHeight);
+        return image.pixels()[mappedX][mappedY];
     }
 }
